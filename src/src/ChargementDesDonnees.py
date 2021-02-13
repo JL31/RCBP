0157fb5d4ffb473aee8645aa9d0754671872c7fe
JL31 <jeplul31@gmail.com>
2021-02-13 15:48:32 +0100

    Cleaning

--- src/src/ChargementDesDonnees.py
+++ src/src/ChargementDesDonnees.py
@@ -5,7 +5,7 @@
 __all__ = ["ChargementDesDonnees"]
 
 # ======================================================================================================================
-# Import des librairies
+# Imports
 # ======================================================================================================================
 
 import os
@@ -21,262 +21,232 @@ class ChargementDesDonnees(object):
         Classe de chargement des données située dans le dossier Donnees
         Le chargement se fait automatiquement à l'ouverture de l'application
     """
-    
+
     def __init__(self, nom_absolu_du_dossier_contenant_les_donnees, fichier_de_donnees, option):
         """
             Constructeur de la classe
         """
-        
+
         self._nom_absolu_du_dossier_contenant_les_donnees = nom_absolu_du_dossier_contenant_les_donnees
         self._fichier_de_donnees = fichier_de_donnees
         self._option = option
-        
+
         self._nom_absolu_du_fichier_de_donnees = ""
-        
-        self._separator = ','
-        self._encoding = 'utf-8'
+
+        self._separator = ","
+        self._encoding = "utf-8"
         self._nombre_de_lignes_a_ignorer = 1
-        
+
         self._date_de_mise_a_jour = None
         self._total_annee_precedente_lui = None
         self._total_annee_precedente_elle = None
-        
+
         self._premiere_ligne_du_fichier = None
-        
+
         self._donnees = None
-        
+
         self._total = None
         self._total_lui = None
         self._total_elle = None
-        
-    
-    ### Accesseurs et mutateurs
-    
+
     def get_nom_absolu_du_dossier_contenant_les_donnees(self):
         """
             Accesseur de l'attribut _nom_absolu_du_dossier_contenant_les_donnees
         """
-        
+
         return self._nom_absolu_du_dossier_contenant_les_donnees
-        
-    
+
     def get_fichier_de_donnees(self):
         """
             Accesseur de l'attribut _fichier_de_donnees
         """
-        
+
         return self._fichier_de_donnees
-        
-    
+
     def get_nom_absolu_du_fichier_de_donnees(self):
         """
             Accesseur de l'attribut _nom_absolu_du_fichier_de_donnees
         """
-        
+
         return self._nom_absolu_du_fichier_de_donnees
-        
-    
+
     def set_nom_absolu_du_fichier_de_donnees(self, valeur):
         """
             Accesseur de l'attribut _nom_absolu_du_fichier_de_donnees
         """
-        
+
         self._nom_absolu_du_fichier_de_donnees = valeur
-        
-    
+
     def get_separator(self):
         """
             Accesseur de l'attribut _separator
         """
-        
+
         return self._separator
-        
-    
+
     def get_donnees(self):
         """
             Accesseur de l'attribut _donnees
         """
-        
+
         return self._donnees
-        
-    
+
     def set_donnees(self, valeurs):
         """
             Mutateur de l'attribut _donnees
         """
-        
+
         self._donnees = valeurs
-        
-    
+
     def get_total(self):
         """
             Accesseur de l'attribut _total
         """
-        
+
         return str(round(self._total, 2))
-        
-    
+
     def set_total(self, valeurs):
         """
             Mutateur de l'attribut _total
         """
-        
+
         self._total = valeurs
-        
-    
+
     def get_total_lui(self):
         """
             Accesseur de l'attribut _total_lui
         """
-        
+
         return str(round(self._total_lui, 2))
-        
-    
+
     def set_total_lui(self, valeurs):
         """
             Mutateur de l'attribut _total_lui
         """
-        
+
         self._total_lui = valeurs
-        
-    
+
     def get_total_elle(self):
         """
             Accesseur de l'attribut _total_elle
         """
-        
+
         return str(round(self._total_elle, 2))
-        
-    
+
     def set_total_elle(self, valeurs):
         """
             Mutateur de l'attribut _total_elle
         """
-        
+
         self._total_elle = valeurs
-        
-    
+
     def get_date_de_mise_a_jour(self):
         """
             Accesseur de l'attribut _date_de_mise_a_jour
         """
-        
+
         return self._date_de_mise_a_jour
-        
-    
+
     def set_date_de_mise_a_jour(self, valeurs):
         """
             Mutateur de l'attribut _date_de_mise_a_jour
         """
         
         self._date_de_mise_a_jour = valeurs
-        
-    
+
     def get_total_annee_precedente_lui(self):
         """
             Accesseur de l'attribut _total_annee_precedente_lui
         """
-        
+
         return self._total_annee_precedente_lui
-        
-    
+
     def set_total_annee_precedente_lui(self, valeurs):
         """
             Mutateur de l'attribut _total_annee_precedente_lui
         """
-        
+
         self._total_annee_precedente_lui = valeurs
-        
-    
+
     def get_total_annee_precedente_elle(self):
         """
             Accesseur de l'attribut _total_annee_precedente_elle
         """
-        
+
         return self._total_annee_precedente_elle
-        
-    
+
     def set_total_annee_precedente_elle(self, valeurs):
         """
             Mutateur de l'attribut _total_annee_precedente_elle
         """
-        
+
         self._total_annee_precedente_elle = valeurs        
-        
-    
-    ### Méthodes
-    
+
     def verification_existence_dossier_et_fichier(self):
         """
             Méthode qui permet de vérifier si le dossier et le fichier contenant les données existent
         """
-        
+
         # On commence par vérifier si le dossier contenant les données existe
-        
         if not os.path.isdir(self._nom_absolu_du_dossier_contenant_les_donnees):
-            
             raise OSError(u"Le dossier '{}' n'existe pas".format(self._nom_absolu_du_dossier_contenant_les_donnees))
-            
+
         # On vérifie ensuite si le fichier existe dans le répertoire de données
-        
         self._nom_absolu_du_fichier_de_donnees = os.path.join(self._nom_absolu_du_dossier_contenant_les_donnees, self._fichier_de_donnees)
-        
+
         if not os.path.isfile(self._nom_absolu_du_fichier_de_donnees):
-            
             raise OSError(u"Le fichier '{}' n'existe pas das le dossier '{}'".format(self._fichier_de_donnees, self._nom_absolu_du_dossier_contenant_les_donnees))
-            
-    
+
     def recuperation_des_donnes(self):
         """
             Méthode qui permet de récupérer les données dans un dataframe
             Les données sont dans un fichier ASCII portant l'extension .donnees
             Les données sont séparées par le séparateur défini en attribut de la classe
         """
-        
+
         # On commence par vérifier que le fichier existe
-        
         self.verification_existence_dossier_et_fichier()
-        
+
         # Si le fichier existe on récupère :
         # - la date de dernière mise-à-jour du fichier
         # - le contenu du fichier que l'on place dans un dataframe
-        
         with open(self._nom_absolu_du_fichier_de_donnees, 'r') as fichier_contenant_les_donnes:
 
             self._premiere_ligne_du_fichier = fichier_contenant_les_donnes.readline()
             self._date_de_mise_a_jour = self._premiere_ligne_du_fichier.split(',')[0].strip()
-            
+
             if self._option in ["balance"]:
-                
+
                 self._total_annee_precedente_lui   = self._premiere_ligne_du_fichier.split(',')[1].strip()
                 self._total_annee_precedente_elle = self._premiere_ligne_du_fichier.split(',')[2].strip()
-                
-        
-        self._donnees = pd.read_csv(self._nom_absolu_du_fichier_de_donnees, sep = self._separator, encoding = self._encoding, skiprows = self._nombre_de_lignes_a_ignorer)
-        
-    
+
+        self._donnees = pd.read_csv(
+            self._nom_absolu_du_fichier_de_donnees,
+            sep=self._separator,
+            encoding=self._encoding,
+            skiprows=self._nombre_de_lignes_a_ignorer
+        )
+
     def calcul_du_total(self):
         """
             Méthode qui permet de calculer le montant total
         """
-        
+
         df_donnees = self._donnees
-        self._total = df_donnees['Montant'].sum()
-        
-    
+        self._total = df_donnees["Montant"].sum()
+
     def calcul_du_total_lui(self):
         """
             Méthode qui permet de calculer le montant total pour lui
         """
-        
+
         df_donnees = self._donnees
-        self._total_lui = df_donnees['Montant lui'].sum()
-        
-    
+        self._total_lui = df_donnees["Montant lui"].sum()
+
     def calcul_du_total_elle(self):
         """
             Méthode qui permet de calculer le montant total pour elle
         """
-        
+
         df_donnees = self._donnees
-        self._total_elle = df_donnees['Montant elle'].sum()
+        self._total_elle = df_donnees["Montant elle"].sum()
--- src/src/ImportCSV.py
+++ src/src/ImportCSV.py
@@ -1,173 +1,163 @@
-﻿# coding: utf-8
+﻿"""
+    Module qui contient la classe ImportCSV pour l'application des comptes
+"""
 
-""" Module qui contient la classe ImportCSV pour l'application des comptes """
-
-
-### Paramètres globaux
-
-__author__ = "Julien LEPAIN"
-__version__ = 1.0
 __all__ = ["ImportCSV"]
 
+# ======================================================================================================================
+# Imports
+# ======================================================================================================================
 
-### Import des librairies
-
-import os
 import pandas as pd
 from datetime import datetime
 
 
-### Définitions des classes
+# ======================================================================================================================
+# Définitions des classes
+# ======================================================================================================================
 
 class ImportCSV(object):
     """
         Classe qui permet d'importer un CSV
     """
-    
+
     def __init__(self, fichier_CSV_a_importer, donnees_deja_chargees, liste_des_libelles_lui, liste_des_libelles_elle):
         """
             Constructeur de la classe
         """
-        
-        self._fichier_CSV_a_importer  = fichier_CSV_a_importer
-        self._donnees_deja_chargees   = donnees_deja_chargees
-        self._liste_des_libelles_lui  = liste_des_libelles_lui
+
+        self._fichier_CSV_a_importer = fichier_CSV_a_importer
+        self._donnees_deja_chargees = donnees_deja_chargees
+        self._liste_des_libelles_lui = liste_des_libelles_lui
         self._liste_des_libelles_elle = liste_des_libelles_elle
-        
+
         self._separator = ';'
         self._nombre_de_lignes_a_ignorer = 7
         self._encoding = 'iso-8859-1'
-        self._colonnes_a_conserver = [u"Date",
-                                      u"Libellé",
-                                      u"Montant(EUROS)"]
+        self._colonnes_a_conserver = [
+            "Date",
+            "Libellé",
+            "Montant(EUROS)"
+        ]
         self._donnees_importees = None
         self._extrait_des_donnees_importees = None
         self._donnees_concatenees = None
-        
-    
-    ### Accesseurs et mutateurs
-    
+
     def get_donnees_importees(self):
         """
             Accesseur de l'attribut _donnees_importees
         """
-        
+
         return self._donnees_importees
-        
-    
+
     def set_donnees_importees(self, valeurs):
         """
             Mutateur de l'attribut _donnees_importees
         """
-        
+
         self._donnees_importees = valeurs
-        
-    
+
     def get_extrait_des_donnees_importees(self):
         """
             Accesseur de l'attribut _extrait_des_donnees_importees
         """
-        
+
         return self._extrait_des_donnees_importees
-        
-    
+
     def set_extrait_des_donnees_importees(self, valeurs):
         """
             Mutateur de l'attribut _extrait_des_donnees_importees
         """
-        
+
         self._extrait_des_donnees_importees = valeurs
-        
-    
+
     def get_donnees_concatenees(self):
         """
             Accesseur de l'attribut _donnees_concatenees
         """
-        
+
         return self._donnees_concatenees
-        
-    
+
     def set_donnees_concatenees(self, valeurs):
         """
             Mutateur de l'attribut _donnees_concatenees
         """
-        
+
         self._donnees_concatenees = valeurs
-        
-    
-    ### Méthodes
-    
+
     def recuperation_des_donnes(self):
         """
             Méthode qui permet de récupérer les données dans un dataframe
             Toutes les options concernant la fonction de lecture du CSV (read_csv) sont définies en attribut de la classe
         """
-        
-        self._donnees_importees = pd.read_csv(self._fichier_CSV_a_importer, sep = self._separator, skiprows = self._nombre_de_lignes_a_ignorer, encoding = self._encoding)
+
+        self._donnees_importees = pd.read_csv(
+            self._fichier_CSV_a_importer,
+            sep=self._separator,
+            skiprows=self._nombre_de_lignes_a_ignorer,
+            encoding=self._encoding
+        )
         # self._donnees_importees = pd.read_csv(self._fichier_CSV_a_importer, sep = self._separator, skiprows = self._nombre_de_lignes_a_ignorer, encoding = self._encoding, decimal = ".")
-        # # # # # self._donnees_importees = pd.read_csv(self._fichier_CSV_a_importer, sep = self._separator, skiprows = 7, encoding='iso-8859-1', usecols = colonnes_a_conserver)
-        
-        # # # # # Les deux lignes suivantes seront sans doute à supprimer une fois que la lecture du CSV en utilisant l'option decimal = "." sera active
-        # # # # #   ==> voir comment faire pour créer un exécutable stand alone avec une version récente de pandas
+        # self._donnees_importees = pd.read_csv(self._fichier_CSV_a_importer, sep = self._separator, skiprows = 7, encoding='iso-8859-1', usecols = colonnes_a_conserver)
+
+        # Les deux lignes suivantes seront sans doute à supprimer une fois que la lecture du CSV en utilisant
+        # l'option decimal = "." sera active
+        #   ==> voir comment faire pour créer un exécutable stand alone avec une version récente de pandas
         self._donnees_importees = self._donnees_importees.apply(lambda x: x.str.replace(',', '.'))
-        self._donnees_importees[u"Montant(EUROS)"] = self._donnees_importees[u"Montant(EUROS)"].astype(float)
-        
+        self._donnees_importees["Montant(EUROS)"] = self._donnees_importees["Montant(EUROS)"].astype(float)
+
         self._donnees_importees = self._donnees_importees[self._colonnes_a_conserver]
-        
-    
+
     def extraction_des_donnees(self):
         """
             Méthode qui permet d'extraire, dans les données lues dans le CSV des données à importer, les données qui n'existent pas dans les données déjà chargées
         """
-        
-        if len(self._donnees_deja_chargees) > 0:
+
         # Les données déjà chargées ne sont pas vides
-        
+        if len(self._donnees_deja_chargees) > 0:
+
             # Récupération de la dernière ligne des données déjà chargées
-            
-            ligne_derniere_donnee_deja_chargee = self._donnees_deja_chargees.loc[0]
+            # ligne_derniere_donnee_deja_chargee = self._donnees_deja_chargees.loc[0]
+            ligne_derniere_donnee_deja_chargee = self._donnees_deja_chargees.iloc[0, :]     # TODO : laquelle des deux ligne ???
 
             # Conversion des dates en objet datetime.strptime avant Vérifications
-            
             derniere_date_donnees_importees = datetime.strptime(self._donnees_importees["Date"].values[-1], "%d/%m/%Y")
             derniere_date_donnee_deja_chargees = datetime.strptime(ligne_derniere_donnee_deja_chargee["Date"], "%d/%m/%Y")
-            
+
             # Vérifications 
-            
-            # if self._donnees_importees["Date"].values[-1] > ligne_derniere_donnee_deja_chargee["Date"]:
-            if derniere_date_donnees_importees > derniere_date_donnee_deja_chargees:
+
             # La dernière date des données à importer est supérieure à la première date des données déjà chargées :
             # on récupère toutes les données à importer
-                
+            if derniere_date_donnees_importees > derniere_date_donnee_deja_chargees:
+
                 self._extrait_des_donnees_importees = self._donnees_importees
-                
+
+            # On récupère, dans les données à importer, les lignes qui sont dans les données à importer mais pas dans
+            # celles déjà chargées
             else:
-            # On récupère, dans les données à importer, les lignes qui sont dans les données à importer mais pas dans celles déjà chargées
 
                 condition1 = self._donnees_importees["Date"] == ligne_derniere_donnee_deja_chargee["Date"]
                 condition2 = self._donnees_importees["Libellé"] == ligne_derniere_donnee_deja_chargee["Libellé"]
                 condition3 = self._donnees_importees["Montant(EUROS)"] == ligne_derniere_donnee_deja_chargee["Montant"]
 
                 liste_des_index = self._donnees_importees[condition1 and condition2 and condition3].index
-                
+
                 indice_pour_extraction = liste_des_index[0] - 1
-                
+
                 # Il est possible qu'il y ait des cas où il y aurait deux fois la même ligne, 
-                # La ligne précédente permet de s'affranchir d'un éventuel problème dans cette situation (on prend la première référence)
-                
-                # self._extrait_des_donnees_importees = self._donnees_importees.ix[:indice_pour_extraction]
+                # La ligne précédente permet de s'affranchir d'un éventuel problème dans cette situation
+                # (on prend la première référence)
                 self._extrait_des_donnees_importees = self._donnees_importees.loc[:indice_pour_extraction, :]
 
-        else:
         # Les données déjà chargées sont vides donc on récupère toutes les données à importer
-            
+        else:
+
             self._extrait_des_donnees_importees = self._donnees_importees
-            
+
         # On initialise les colonnes Montant lui et Montant elle
-        
-        self._extrait_des_donnees_importees["Montant lui"]  = 0.0
+
+        self._extrait_des_donnees_importees["Montant lui"] = 0.0
         self._extrait_des_donnees_importees["Montant elle"] = 0.0
-        
     
     def traitement_des_donnees_importees(self):
         """
